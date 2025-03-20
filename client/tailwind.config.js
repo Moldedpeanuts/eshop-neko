926e9599a38efbaf4792153ea01b21763ecdd78b
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-03-20 13:56:03 +0200

    Create client file and configure tailwind config file

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config}*/
+
+module_exports = {
+    content: [
+        "./src/**/*.{html,js,jsx,ts,tsx}",
+        "./public/index.html",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
