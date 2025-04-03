b31cfa259a9d127c77f45996aa05e5a6a13bafd1
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-03 17:12:01 +0300

    Change focus color for input

--- client/src/components/Shared/Footer.jsx
+++ client/src/components/Shared/Footer.jsx
@@ -4,8 +4,7 @@ import Button from '../Button/Button.jsx';
 export default function Footer() {
     return (
         <footer className="bg-primary p-6">
-            <div className="container mx-auto text-center">
-                <div className="mb-6">
+                <div className="px-4 sm:px-0">
                     <h3 className="text-2xl text-white mb-4">
                         Stay Updated
                     </h3>
@@ -13,12 +12,11 @@ export default function Footer() {
                         <input 
                             type="email" 
                             placeholder="Enter Your Email"
-                            className="px-4 py-2 w-full sm:w-auto border border-gray-300 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            className="px-4 py-2 sm:w-auto border border-gray-300 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-secondary"
                         />
                         <Button variant="outline">Subscribe</Button>
                     </form>
                 </div>
-            </div>
         </footer>
     );
 }
