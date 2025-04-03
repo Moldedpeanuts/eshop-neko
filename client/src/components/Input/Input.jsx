e1077a12a9dc40f7f5d9d0d0e41b5ff35ef41373
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-03 14:22:46 +0300

    Remove input component

--- client/src/components/Input/Input.jsx
+++
@@ -1,9 +0,0 @@
-import Button from '../Button/Button.jsx';
-
-export default function Input(type = "text", placeholder, name, value, onSubmit  ) {
-    return(
-        <>
-            <input/><Button size="lg">test</Button>
-        </>
-    );
-}
