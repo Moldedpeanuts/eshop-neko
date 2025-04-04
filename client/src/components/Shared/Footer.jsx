df44fb1ca0df1ce14e13caf5b99dd85c3eb21279
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-04 10:08:55 +0300

    Correct regex object

--- client/src/components/Shared/Footer.jsx
+++ client/src/components/Shared/Footer.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Button from '../Button/Button.jsx';
  
 export default function Footer(value, onSubmit, onChange) {
-    const regex = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
     const [email, setEmail] = useState('');
     const [isValidEmail, setIsValidEmail] = useState(true);
@@ -18,13 +18,17 @@ export default function Footer(value, onSubmit, onChange) {
         }
     };
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
         <footer className="w-full bg-primary p-6">
                 <div className="w-full px-4">
                     <h3 className="text-3xl text-white mb-4 text-center">
                         Subscribe to our Newsletter
                     </h3>
-                    <form className="flex items-center justify-center space-x-2 w-full">
+                    <form className="flex items-center justify-center space-x-2 w-full" onSubmit={handleSubmit}>
                         <input 
                             type="email" 
                             placeholder="Enter Your Email"
@@ -34,7 +38,7 @@ export default function Footer(value, onSubmit, onChange) {
                             value={email}
                         />
                         { isValidEmail ? null : <p style={{color: 'red'}}>Please enter a valid email</p> }
-                        <Button variant="outline" onSubmit={handleSubmit}>Subscribe</Button>
+                        <Button variant="outline">Subscribe</Button>
                     </form>
                 </div>
         </footer>
