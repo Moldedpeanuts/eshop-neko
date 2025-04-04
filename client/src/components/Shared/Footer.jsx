788fe0e6f2b8fe0fa6f3f17dbf99934fcc533fee
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-04 09:16:14 +0300

    Make email input filed required

--- client/src/components/Shared/Footer.jsx
+++ client/src/components/Shared/Footer.jsx
@@ -3,16 +3,17 @@ import Button from '../Button/Button.jsx';
  
 export default function Footer() {
     return (
-        <footer className="bg-primary p-6">
-                <div className="px-4 sm:px-0">
-                    <h3 className="text-2xl text-white mb-4">
-                        Stay Updated
+        <footer className="w-full bg-primary p-6">
+                <div className="w-full px-4">
+                    <h3 className="text-3xl text-white mb-4 text-center">
+                        Subscribe to our Newsletter
                     </h3>
-                    <form className="flex items-center justify-center space-x-2">
+                    <form className="flex items-center justify-center space-x-2 w-full">
                         <input 
                             type="email" 
                             placeholder="Enter Your Email"
-                            className="px-4 py-2 sm:w-auto border border-gray-300 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-secondary"
+                            className="px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-secondary"
+                            required
                         />
                         <Button variant="outline">Subscribe</Button>
                     </form>
