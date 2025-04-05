e032218d36415733f9ad0f07aa9c223ad96b936d
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-05 15:40:15 +0300

    Fix: correct export default for HeartIcon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,22 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Button from './components/Button/Button.jsx'
 import Footer from './components/Shared/Footer.jsx'
+import HeartIcon  from './components/HeartIcon/HeartIcon.jsx'
 import { BsCart3 } from "react-icons/bs"; // cart icon
-import { CiHeart } from "react-icons/ci"; // empty heart icon
-import { FaHeart } from "react-icons/fa"; // full heart icon
 import { CiSearch } from "react-icons/ci"; // search icon
 import { FaPhoneAlt } from "react-icons/fa"; // phone icon
 import { FaRegUser } from "react-icons/fa6"; // user icon
 
-
-
-
-
-
 export default function App() {
   return (
     <>
-      <CiHeart></CiHeart>
+      <CiSearch></CiSearch>
     </>
   );
 }
