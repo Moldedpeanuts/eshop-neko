f3773165fcc27349943fecc94404a509e8634399
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-05 15:27:54 +0300

    Add full heart icon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,8 @@ import './App.css'
 import Button from './components/Button/Button.jsx'
 import Footer from './components/Shared/Footer.jsx'
 import { BsCart3 } from "react-icons/bs"; // cart icon
-import { MdFavoriteBorder } from "react-icons/md"; // heart icon empty
+import { CiHeart } from "react-icons/ci"; // empty heart icon
+import { FaHeart } from "react-icons/fa"; // full heart icon
 import { CiSearch } from "react-icons/ci"; // search icon
 import { FaPhoneAlt } from "react-icons/fa"; // phone icon
 import { FaRegUser } from "react-icons/fa6"; // user icon
@@ -18,7 +19,7 @@ import { FaRegUser } from "react-icons/fa6"; // user icon
 export default function App() {
   return (
     <>
-      <FaRegUser></FaRegUser>
+      <CiHeart></CiHeart>
     </>
   );
 }
