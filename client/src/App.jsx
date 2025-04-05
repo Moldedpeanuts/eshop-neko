6ca078470ef534e04abf59711d3fdf7a6b1bb193
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-05 15:22:25 +0300

    Import icons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Button from './components/Button/Button'
+import Button from './components/Button/Button.jsx'
+import Footer from './components/Shared/Footer.jsx'
+import { BsCart3 } from "react-icons/bs"; // cart icon
+import { MdFavoriteBorder } from "react-icons/md"; // heart icon empty
+import { CiSearch } from "react-icons/ci"; // search icon
+import { FaPhoneAlt } from "react-icons/fa"; // phone icon
+import { FaRegUser } from "react-icons/fa6"; // user icon
+
+
+
+
+
 
 export default function App() {
   return (
     <>
-      <Button size="lg">test123</Button>
+      <FaRegUser></FaRegUser>
     </>
   );
 }
