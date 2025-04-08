0322c772afc404821bf356b46a21ba7b3cf50613
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-08 14:54:21 +0300

    Add container div

---
+++ client/src/components/ItemCard/ItemCard.jsx
@@ -0,0 +1,12 @@
+import Button from '../Button/Button.jsx';
+import { BsCart3 } from 'react-icons/bs'; // cart icon
+
+export default function ItemCard() {
+    return(
+        <>
+            <div className="card-container">
+
+            </div>
+        </>
+    );
+}
