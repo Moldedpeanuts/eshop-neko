e6c4aa7f93439cca296b30b75d135d440ca6b3af
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-08 15:47:03 +0300

    Add test image

--- client/src/components/ItemCard/ItemCard.jsx
+++ client/src/components/ItemCard/ItemCard.jsx
@@ -1,13 +1,13 @@
 import Button from '../Button/Button.jsx';
-import HeartIcon  from './components/HeartIcon/HeartIcon.jsx'
+import HeartIcon  from '../HeartIcon/HeartIcon.jsx';
 
 export default function ItemCard({imgSource, title, price}) {
     return(
         <>
             <div className="card-container h-96 w-64 bg-white">
-                <img src={imgSource} alt={title}/>
+                <img src="https://unionarena.pokeninjapan.store/cdn/shop/files/img_item01_1_ef1a93fd-5df3-49f5-8ab6-7823ac7647a4.png?v=1732773121&width=800" alt={title}/>
                 <h3>Item Title</h3>
-                
+                <HeartIcon></HeartIcon>
                 <p>price $</p>
                 <Button>Add to Cart</Button>
             </div>
