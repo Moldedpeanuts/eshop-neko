2f1ceb906a00fac4b9e9cab3385c688a1c527b1b
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-08 15:25:39 +0300

    Add item card component props

--- client/src/components/ItemCard/ItemCard.jsx
+++ client/src/components/ItemCard/ItemCard.jsx
@@ -1,11 +1,14 @@
 import Button from '../Button/Button.jsx';
 import { BsCart3 } from 'react-icons/bs'; // cart icon
 
-export default function ItemCard() {
+export default function ItemCard({imgSource, title, price}) {
     return(
         <>
             <div className="card-container h-96 w-64 bg-white">
-                dasdad
+                <img src={imgSource} alt={title}/>
+                <h3>{title}</h3>
+                <p>{price}</p>
+                <Button>Add to Cart</Button>
             </div>
         </>
     );
