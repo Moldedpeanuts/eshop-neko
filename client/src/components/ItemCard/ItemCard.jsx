e2b9d51c137432128a083afb30339cfced66827c
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-08 16:12:33 +0300

    Add bottom padding to image

--- client/src/components/ItemCard/ItemCard.jsx
+++ client/src/components/ItemCard/ItemCard.jsx
@@ -5,12 +5,12 @@ export default function ItemCard({imgSource, title, price}) {
     return(
         <>
             <div className="card-container h-96 w-64 bg-white p-3">
-                <img src="https://unionarena.pokeninjapan.store/cdn/shop/files/img_item01_1_ef1a93fd-5df3-49f5-8ab6-7823ac7647a4.png?v=1732773121&width=800" alt={title}/>
+                <img src="https://unionarena.pokeninjapan.store/cdn/shop/files/img_item01_1_ef1a93fd-5df3-49f5-8ab6-7823ac7647a4.png?v=1732773121&width=800" className="pb-3" alt={title}/>
                 <div className="flex justify-between">
                     <h3 className="font-semibold text-sm">Item Title</h3>
                     <HeartIcon></HeartIcon>
                 </div>
-                <p className="font-semibold text-left pt-4">7,99$</p>
+                <p className="font-extrabold text-left pt-4">7,99$</p>
                 <Button>Add to Cart</Button>
             </div>
         </>
