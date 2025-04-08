e4e2257c9a012040011c6a973b25662d8b3835c5
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-08 15:08:10 +0300

    Add width to card container

--- client/src/components/ItemCard/ItemCard.jsx
+++ client/src/components/ItemCard/ItemCard.jsx
@@ -4,7 +4,7 @@ import { BsCart3 } from 'react-icons/bs'; // cart icon
 export default function ItemCard() {
     return(
         <>
-            <div className="card-container border-2 border-indigo-400 h-80">
+            <div className="card-container border-2 border-indigo-400 h-96 w-64">
                 dasdad
             </div>
         </>
