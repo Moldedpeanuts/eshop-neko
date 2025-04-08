ede9c513105090eb9a28f8b43bde4549db59084a
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-08 16:02:20 +0300

    Add padding to whole container

--- client/src/components/ItemCard/ItemCard.jsx
+++ client/src/components/ItemCard/ItemCard.jsx
@@ -4,7 +4,7 @@ import HeartIcon  from '../HeartIcon/HeartIcon.jsx';
 export default function ItemCard({imgSource, title, price}) {
     return(
         <>
-            <div className="card-container h-96 w-64 bg-white">
+            <div className="card-container h-96 w-64 bg-white p-3">
                 <img src="https://unionarena.pokeninjapan.store/cdn/shop/files/img_item01_1_ef1a93fd-5df3-49f5-8ab6-7823ac7647a4.png?v=1732773121&width=800" alt={title}/>
                 <div className="flex justify-between">
                     <h3 className="font-semibold">Item Title</h3>
