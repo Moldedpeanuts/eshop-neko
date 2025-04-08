fd46f5cbaeb71676ddbed519f89d80997ba948b8
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-08 15:35:51 +0300

    Fix: import heart icon component

--- client/src/components/ItemCard/ItemCard.jsx
+++ client/src/components/ItemCard/ItemCard.jsx
@@ -1,13 +1,14 @@
 import Button from '../Button/Button.jsx';
-import { BsCart3 } from 'react-icons/bs'; // cart icon
+import HeartIcon  from './components/HeartIcon/HeartIcon.jsx'
 
 export default function ItemCard({imgSource, title, price}) {
     return(
         <>
             <div className="card-container h-96 w-64 bg-white">
                 <img src={imgSource} alt={title}/>
-                <h3>{title}</h3>
-                <p>{price}</p>
+                <h3>Item Title</h3>
+                
+                <p>price $</p>
                 <Button>Add to Cart</Button>
             </div>
         </>
