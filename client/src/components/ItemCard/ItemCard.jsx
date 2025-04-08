ed308858f29211d070b81ea52276ca1eb2605110
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-08 15:58:13 +0300

    Wrap title and heart icon to an extra flex container

--- client/src/components/ItemCard/ItemCard.jsx
+++ client/src/components/ItemCard/ItemCard.jsx
@@ -6,8 +6,10 @@ export default function ItemCard({imgSource, title, price}) {
         <>
             <div className="card-container h-96 w-64 bg-white">
                 <img src="https://unionarena.pokeninjapan.store/cdn/shop/files/img_item01_1_ef1a93fd-5df3-49f5-8ab6-7823ac7647a4.png?v=1732773121&width=800" alt={title}/>
-                <h3>Item Title</h3>
-                <HeartIcon></HeartIcon>
+                <div className="flex ">
+                    <h3 className="inline">Item Title</h3>
+                    <HeartIcon></HeartIcon>
+                </div>
                 <p>price $</p>
                 <Button>Add to Cart</Button>
             </div>
