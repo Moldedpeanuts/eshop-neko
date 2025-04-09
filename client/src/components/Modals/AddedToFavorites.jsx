9943d22c3abf50a25f9db0881804a4a4ef4197cc
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-09 14:49:05 +0300

    Style pop up favorites modal

---
+++ client/src/components/Modals/AddedToFavorites.jsx
@@ -0,0 +1,11 @@
+export default function AddedToFavorites() {
+    return(
+        <>
+            <div className="flex flex-wrap items-center justify-center w-48 h-24 bg-[rgba(161,140,209,0.6)] text-white p-2 text-center rounded-xl animate-fadeIn">
+                <p break words overflow-hidden>
+                    Item added to Favorites!
+                </p>
+            </div>
+        </>
+    );
+}
