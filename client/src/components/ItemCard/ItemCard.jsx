b62721ce25e2214f09c9b84321835e2e8fe6c048
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-12 10:54:40 +0300

    Add hard coded image for ui

--- client/src/components/ItemCard/ItemCard.jsx
+++ client/src/components/ItemCard/ItemCard.jsx
@@ -16,7 +16,7 @@ export default function ItemCard({imgSource, title, price}) {
     return(
         <>
             <div className="card-container h-96 w-64 bg-white p-3">
-                <img src={imgSource} className="pb-3" alt={title}/>
+                <img src="https://iconnectstore.gr/image/cache/catalog/iConnect%20Images/04%20new/KOREAN/Pok%C3%A9mon%20TCG%20Scarlet%20and%20Violet%20151%20Booster%20Box%20-%20Korean%20Lang.7-600x600.jpg" className="pb-3" alt={title}/>
                 <div className="flex justify-between">
                     <h3 className="font-semibold text-sm">{title}</h3>
                     <HeartIcon onClick={handleClick}></HeartIcon>
@@ -24,7 +24,7 @@ export default function ItemCard({imgSource, title, price}) {
                 <p className="font-extrabold text-left pt-4 pb-3">{price}</p>
                 <Button>Add to Cart</Button>
             </div>
-            { isFavoriteModalOpen && <AddedToFavorites></AddedToFavorites>}
+            {isFavoriteModalOpen && <AddedToFavorites></AddedToFavorites>}
         </>
     );
 }
