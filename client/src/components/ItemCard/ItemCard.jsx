87ab9e9831701d0ba019b4dc79cfbcfc274f1614
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-12 10:25:17 +0300

    Change hard coded values of img and h3 to dynamic props

--- client/src/components/ItemCard/ItemCard.jsx
+++ client/src/components/ItemCard/ItemCard.jsx
@@ -1,7 +1,8 @@
+import { useState } from 'react';
 import Button from '../Button/Button.jsx';
 import HeartIcon  from '../HeartIcon/HeartIcon.jsx';
 import AddedToFavorites from '../Modals/AddedToFavorites.jsx';
-import { useState } from 'react';
+
 
 export default function ItemCard({imgSource, title, price}) {
     const [isFavoriteModalOpen, setIsFavoriteModalOpen] = useState(false);
@@ -17,7 +18,7 @@ export default function ItemCard({imgSource, title, price}) {
             <div className="card-container h-96 w-64 bg-white p-3">
                 <img src={imgSource} className="pb-3" alt={title}/>
                 <div className="flex justify-between">
-                    <h3 className="font-semibold text-sm">Item Title</h3>
+                    <h3 className="font-semibold text-sm">{title}</h3>
                     <HeartIcon onClick={handleClick}></HeartIcon>
                 </div>
                 <p className="font-extrabold text-left pt-4 pb-3">7,99$</p>
