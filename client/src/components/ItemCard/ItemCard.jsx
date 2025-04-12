564cdd957a09be76eb48ecc74dcbbe7de38c8835
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-12 10:23:47 +0300

    Add state to favorite modal

--- client/src/components/ItemCard/ItemCard.jsx
+++ client/src/components/ItemCard/ItemCard.jsx
@@ -1,18 +1,29 @@
 import Button from '../Button/Button.jsx';
 import HeartIcon  from '../HeartIcon/HeartIcon.jsx';
+import AddedToFavorites from '../Modals/AddedToFavorites.jsx';
+import { useState } from 'react';
 
 export default function ItemCard({imgSource, title, price}) {
+    const [isFavoriteModalOpen, setIsFavoriteModalOpen] = useState(false);
+
+    const handleClick = () => {
+        setIsFavoriteModalOpen(true);
+
+        setTimeout(() => setIsFavoriteModalOpen(false), 2000);
+    };
+
     return(
         <>
             <div className="card-container h-96 w-64 bg-white p-3">
-                <img src="https://unionarena.pokeninjapan.store/cdn/shop/files/img_item01_1_ef1a93fd-5df3-49f5-8ab6-7823ac7647a4.png?v=1732773121&width=800" className="pb-3" alt={title}/>
+                <img src={imgSource} className="pb-3" alt={title}/>
                 <div className="flex justify-between">
                     <h3 className="font-semibold text-sm">Item Title</h3>
-                    <HeartIcon></HeartIcon>
+                    <HeartIcon onClick={handleClick}></HeartIcon>
                 </div>
                 <p className="font-extrabold text-left pt-4 pb-3">7,99$</p>
                 <Button>Add to Cart</Button>
             </div>
+            { isFavoriteModalOpen && <AddedToFavorites></AddedToFavorites>}
         </>
     );
 }
