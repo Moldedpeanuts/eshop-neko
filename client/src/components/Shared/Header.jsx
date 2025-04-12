424387e819530f29cbceb209e0de34cbf2a90520
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-12 12:00:03 +0300

    Style phone div

--- client/src/components/Shared/Header.jsx
+++ client/src/components/Shared/Header.jsx
@@ -1,8 +1,11 @@
+import { FaPhoneAlt } from "react-icons/fa"; // phone icon
 import Nav from '../Nav/Nav.jsx'
 
 export default function Header() {
     return(
         <header className="bg-primary text-white py-20">
+            <div className="flex justify-start gap-4 align-center px-4 font-bold"><FaPhoneAlt></FaPhoneAlt>211 9000122</div>
+            <h1 className="text-left px-5"><a>NEKO</a></h1>
             <div className="container mx-auto flex justify-between items-center">
                 <Nav></Nav>
             </div>
