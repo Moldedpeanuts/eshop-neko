b7041ac99b887025806a864274e86e02f978bb80
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-12 11:47:34 +0300

    Import nav to header

---
+++ client/src/components/Nav/Nav.jsx
@@ -0,0 +1,7 @@
+export default function Nav() {
+    return(
+        <>
+            <h3>test</h3>
+        </>
+    );
+}
--- client/src/components/Shared/Header.jsx
+++ client/src/components/Shared/Header.jsx
@@ -1,8 +1,10 @@
+import Nav from '../Nav/Nav.jsx'
+
 export default function Header() {
     return(
         <header className="bg-primary text-white py-20">
             <div className="container mx-auto flex justify-between items-center">
-                sdfsfsdf
+                <Nav></Nav>
             </div>
         </header>
     );
