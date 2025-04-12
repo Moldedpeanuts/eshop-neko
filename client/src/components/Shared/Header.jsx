a984775d797631ce67052bb7ab8308f64463947a
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-12 12:56:37 +0300

    Make telephone icon bigger

--- client/src/components/Shared/Header.jsx
+++ client/src/components/Shared/Header.jsx
@@ -4,7 +4,7 @@ import Nav from '../Nav/Nav.jsx'
 export default function Header() {
     return(
         <header className="bg-primary text-white">
-            <div className="flex justify-start gap-4 align-center py-4 px-4 font-bold"><FaPhoneAlt></FaPhoneAlt>211 9000122</div>
+            <div className="flex justify-start gap-4 align-center py-4 px-4 font-bold"><FaPhoneAlt className="text-xl"></FaPhoneAlt>211 9000122</div>
             <div className="border-t border-secondary my-4"></div>
             <Nav></Nav>
         </header>
