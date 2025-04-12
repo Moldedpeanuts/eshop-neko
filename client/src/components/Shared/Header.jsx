cd13c574c646b50b9ed837d29087ba81fc1695e0
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-12 12:33:52 +0300

    Make icon utilities bigger

--- client/src/components/Shared/Header.jsx
+++ client/src/components/Shared/Header.jsx
@@ -3,13 +3,10 @@ import Nav from '../Nav/Nav.jsx'
 
 export default function Header() {
     return(
-        <header className="bg-primary text-white py-20">
-            <div className="flex justify-start gap-4 align-center px-4 font-bold"><FaPhoneAlt></FaPhoneAlt>211 9000122</div>
+        <header className="bg-primary text-white">
+            <div className="flex justify-start gap-4 align-center py-4 px-4 font-bold"><FaPhoneAlt></FaPhoneAlt>211 9000122</div>
             <div className="border-t border-secondary my-4"></div>
-            <h1 className="text-left px-5"><a>NEKO</a></h1>
-            <div className="container mx-auto flex justify-between items-center">
-                <Nav></Nav>
-            </div>
+            <Nav></Nav>
         </header>
     );
 }
