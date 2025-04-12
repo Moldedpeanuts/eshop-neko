6c33a978855779c6e9fdab6b2d5447b93eff7d89
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-12 12:38:58 +0300

    Finish styling logo

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -2,6 +2,7 @@ import { BsCart3 } from "react-icons/bs"; // cart icon
 import { CiSearch } from "react-icons/ci"; // search icon
 import { FaRegUser } from "react-icons/fa6"; // user icon
 import { FaRegHeart } from "react-icons/fa"; // heart icon
+import { FaCat } from "react-icons/fa"; // cat logo
 
 export default function Nav() {
     return(
@@ -10,7 +11,7 @@ export default function Nav() {
                 <div className="grid grid-cols-3 gap-4 items-center">
                     {/* Logo Section */}
                     <div className="flex justify-start">
-                        <h1 className="text-3xl font-bold">NEKO</h1>
+                        <FaCat className="text-2xl cursor-pointer"></FaCat><h1 className="text-5xl font-bold cursor-pointer">NEKO</h1>
                     </div>
 
                     {/* Search Bar Section */}
@@ -18,15 +19,15 @@ export default function Nav() {
                         <input
                             type="text"
                             placeholder="Search..."
-                            className="px-4 py-2 w-64 rounded-lg border border-gray-300"
+                            className="px-4 py-2 w-64 rounded-lg border border-gray-300 text-black"
                         />
                     </div>
 
                     {/* Profile, Favorites and Cart section */}
                     <div className="flex justify-end space-x-6 gap-3">
-                        <a href="#"><FaRegUser></FaRegUser></a>
-                        <a href="#"><FaRegHeart></FaRegHeart></a>
-                        <a href="#"><BsCart3></BsCart3></a>
+                        <a href="#"><FaRegUser className="text-2xl"></FaRegUser></a>
+                        <a href="#"><FaRegHeart className="text-2xl"></FaRegHeart></a>
+                        <a href="#"><BsCart3 className="text-2xl"></BsCart3></a>
                     </div>
                 </div>
             </div>
