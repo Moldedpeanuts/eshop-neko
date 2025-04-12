c94afa54413aa524cb1d23024545eb3035588f15
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-12 12:17:17 +0300

    Remove icons and transfer to nav component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,6 @@ import './App.css'
 import Button from './components/Button/Button.jsx'
 import Footer from './components/Shared/Footer.jsx'
 import HeartIcon  from './components/HeartIcon/HeartIcon.jsx'
-import { BsCart3 } from "react-icons/bs"; // cart icon
-import { CiSearch } from "react-icons/ci"; // search icon
-import { FaRegUser } from "react-icons/fa6"; // user icon
 import ItemCard from './components/ItemCard/ItemCard.jsx'
 import Header from './components/Shared/Header.jsx'
 
