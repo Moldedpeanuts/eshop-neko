5d833498600d768db0ce536c0a235851f5c8c0e8
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-12 11:43:10 +0300

    Add more padding to header

--- client/src/components/Shared/Header.jsx
+++ client/src/components/Shared/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return(
-        <header className="bg-primary text-white py-4">
+        <header className="bg-primary text-white py-20">
             <div className="container mx-auto flex justify-between items-center">
                 sdfsfsdf
             </div>
