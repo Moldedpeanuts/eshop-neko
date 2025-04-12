215f2d5aa2ec8a59c840565c117a32e43299200f
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-12 11:15:11 +0300

    Add basic test text for header component

---
+++ client/src/components/Shared/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return(
+        <header className="bg-primary text-white py-4">
+            <div className="container mx-auto flex justify-between items-center">
+                sdfsfsdf
+            </div>
+        </header>
+    );
+}
