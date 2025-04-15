50fd8c9627e99d857709dc829325dc3620bc163a
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-15 14:14:23 +0300

    Remove the two ad banners

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Button from './components/Button/Button.jsx'
@@ -7,11 +6,16 @@ import Footer from './components/Shared/Footer.jsx'
 import HeartIcon  from './components/HeartIcon/HeartIcon.jsx'
 import ItemCard from './components/ItemCard/ItemCard.jsx'
 import Header from './components/Shared/Header.jsx'
+import AdBanner from './components/AdBanners/AdBanner1.jsx'
+import Carousel from './components/Carousel/Carousel.jsx'
 
 export default function App() {
   return (
     <>
       <Header></Header>
+      <Carousel></Carousel>
+      <AdBanner></AdBanner>
+      <Footer></Footer>
     </>
   );
 }
