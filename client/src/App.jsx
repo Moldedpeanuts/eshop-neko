3b5649fc675b14ed77daf35c4db1307ecdfc4c6d
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-15 14:49:51 +0300

    ../App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,15 +6,21 @@ import Footer from './components/Shared/Footer.jsx'
 import HeartIcon  from './components/HeartIcon/HeartIcon.jsx'
 import ItemCard from './components/ItemCard/ItemCard.jsx'
 import Header from './components/Shared/Header.jsx'
-import AdBanner from './components/AdBanners/AdBanner1.jsx'
+import AdBanner1 from './components/AdBanners/AdBanner1.jsx'
+import AdBanner2 from './components/AdBanners/AdBanner2.jsx'
 import Carousel from './components/Carousel/Carousel.jsx'
 
 export default function App() {
   return (
     <>
       <Header></Header>
+      <div className="mb-8">
         <Carousel></Carousel>
-      <AdBanner></AdBanner>
+      </div>
+      <AdBanner1></AdBanner1>
+      <div className="mt-32 mb-32">
+        <AdBanner2></AdBanner2>
+      </div>
       <Footer></Footer>
     </>
   );
