00248c40dbb0e3ba78e5d3bc9d088dd5df863697
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-15 14:09:49 +0300

    Make container smaller

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -12,16 +12,16 @@ export default function Carousel() {
             navigation
             autoplay={{delay: 3000}}
             loop={true}
-            className="swiper"
+            className="swiper h-96"
         >
             <SwiperSlide>
-                <img src='https://cloudcitygames.com/wp-content/uploads/Content/Product-Banners/funko-pop-banner.png' alt='first slide' className='w-full object-cover'/>
+                <img src='https://cloudcitygames.com/wp-content/uploads/Content/Product-Banners/funko-pop-banner.png' alt='first slide' className='w-full h-full object-cover'/>
             </SwiperSlide>
             <SwiperSlide>
-                <img src='https://i.ytimg.com/vi/qGe2tWu2J3w/maxresdefault.jpg' alt='second slide' className='w-full object-cover'/>
+                <img src='https://i.ytimg.com/vi/qGe2tWu2J3w/maxresdefault.jpg' alt='second slide' className='w-full h-full object-cover'/>
             </SwiperSlide>
             <SwiperSlide>
-                <img src='https://static0.gamerantimages.com/wordpress/wp-content/uploads/2023/04/10-best-spellcaster-type-yu-gi-oh-card-designs-ranked.jpg' alt='third slide' className='object-cover'/>
+                <img src='https://static0.gamerantimages.com/wordpress/wp-content/uploads/2023/04/10-best-spellcaster-type-yu-gi-oh-card-designs-ranked.jpg' h-full alt='third slide' className='object-cover'/>
             </SwiperSlide>
         </Swiper>
     );
