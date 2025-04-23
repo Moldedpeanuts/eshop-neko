27dddd66da5750c45d729376c24d7f6905a3eaa9
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-04-23 22:44:47 +0300

    Set up another get route and test html

--- server/myapp/server.js
+++ server/myapp/server.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('test!');
+    res.send('test213412!');
+});
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About</h1>');
 });
 
 app.listen(port, () => {
