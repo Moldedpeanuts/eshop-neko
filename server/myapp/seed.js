302d63fd1a180b7b10212cb6daa3422d63008a36
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-05-01 18:42:49 +0300

    Seed faker data

--- server/myapp/seed.js
+++ server/myapp/seed.js
@@ -2,3 +2,22 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const Product = require('./models/Product');
 const { faker } = require('@faker-js/faker');
+
+async function seedDatabase() {
+    await mongoose.connect(process.env.MONGO_URI);
+    await Product.deleteMany();
+
+    const products = [];
+
+    for(let i = 0; i < 50; i++) {
+        products.push({
+            name: faker.commerce.productName(),
+            price: parseFloat(faker.commerce.price({min: 5, max:100})),
+            description: faker.commerce.productDescription(),
+            imageUrl: faker.image.urlPicsumPhotos(),
+            category: faker.commerce.department(),
+            stockQuantity: faker.number.int({ min: 0, max: 100}),
+            creation: faker.date.past(),
+        });
+    }
+}
