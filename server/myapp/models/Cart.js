faf5a84fb90caea274f27fc6a712885f827c89f3
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-05-01 19:23:19 +0300

    Add cart item schema based on product model

--- server/myapp/models/Cart.js
+++ server/myapp/models/Cart.js
@@ -1,3 +1,7 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+const cartItemSchema = new Schema({
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true},
+    quantity: { type: Number, default: 1},
+});
