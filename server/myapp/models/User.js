0f2e834a5419c6960ad31daac3c2b4e1fd0beaeb
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-05-02 15:09:23 +0300

    Add unique property and set to true for email property

--- server/myapp/models/User.js
+++ server/myapp/models/User.js
@@ -3,6 +3,6 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
 });
