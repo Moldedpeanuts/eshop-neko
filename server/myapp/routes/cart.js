f80c205ef5a88e8041b9d863c36894c8b2e91cda
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-05-02 12:08:54 +0300

    Handle case when a cart for request isnt found and create such cart

--- server/myapp/routes/cart.js
+++ server/myapp/routes/cart.js
@@ -5,4 +5,10 @@ const Cart = require('../models/Cart');
 router.post(async (req, res) => {
     const { productId, quantity} = req.body;
     let cart = await Cart.findOne();
+
+    if(!cart) {
+        const cart = await Cart.create({
+            items: [{ product: productId, quantity }],
+        });
+    }
 });
