4f6a6e9418645c393f1ec53686b00c650a4595f1
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-05-03 14:02:19 +0300

    Set property to not create a session until something is stored

--- server/myapp/middleware/session.js
+++ server/myapp/middleware/session.js
@@ -3,4 +3,6 @@ const session = require('express-session');
 const sessionMiddleware = session({
     secret: process.env.SESSION_SECRET,
     resave: false,
+    saveUninitialized: false,
+    
 });
