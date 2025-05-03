aacf8269319fb5567084d782e083c16cf9bfed3a
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-05-03 15:52:01 +0300

    Include try catch block to pass check for error handling

--- server/myapp/routes/login.js
+++ server/myapp/routes/login.js
@@ -5,5 +5,16 @@ const { comparePassword } = require('../utils/password');
 
 router.post('/login', async(req, res) => {
     const { email, password } = req.body;
-    const user = await User.findOne({ email });
+    try {
+        const user = await User.findOne({ email });
+        const passMatch = await comparePassword(password, user.password);
+        if(passMatch) {
+            res.status(200).json({ message: 'Login successful!'});
+        } else {
+            res.send.json({ error: 'User was not found'});
+        }
+    } catch(err) {
+        console.error('User not found with error:', err);
+        res.status(500).json({ error: 'Internal server error'});
+    }
 });
