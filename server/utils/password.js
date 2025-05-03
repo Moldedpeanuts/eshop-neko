76158379d8fce78311383b85ac27f3bd9584d111
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-05-03 15:26:17 +0300

    Export comparePassword async function

--- server/utils/password.js
+++ server/utils/password.js
@@ -1,3 +1,7 @@
 const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const hash = await bcrypt.hash(password, saltRounds);
+
+async function comparePassword(password, hash) {
+    return await bcrypt.compare(password, hash);
+}
+
+module.exports = { comparePassword };
