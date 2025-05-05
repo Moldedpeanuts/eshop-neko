8e5fa3bb5e5af435f210669ce91a5b9aef7b2053
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-05-05 13:13:25 +0300

    Connect User model to Cart model

--- server/myapp/models/Cart.js
+++ server/myapp/models/Cart.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const cartItemSchema = new Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
     product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true},
     quantity: { type: Number, default: 1},
 });
