9788a7af26f4be2e39be28bd057bac125e4d932a
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-05-06 15:02:01 +0300

    Import User model

--- server/myapp/routes/registration.js
+++ server/myapp/routes/registration.js
@@ -1,3 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { comparePassword } = require('../utils/password'); s
+const User = require('../models/User');
+const { comparePassword } = require('../utils/password');
+
