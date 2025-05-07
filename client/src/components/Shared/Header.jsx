a4e056bb0131d0eb2b7c8cf2af6ea10870470e53
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-05-07 19:39:02 +0300

    Wrap Nav component in a div

--- client/src/components/Shared/Header.jsx
+++ client/src/components/Shared/Header.jsx
@@ -3,10 +3,12 @@ import Nav from '../Nav/Nav.jsx'
 
 export default function Header() {
     return(
-        <header className="bg-primary text-white">
+        <header className="bg-primary text-white flex flex-col">
             <div className="flex justify-start gap-4 align-center py-4 px-4 font-bold"><FaPhoneAlt className="text-xl"></FaPhoneAlt>211 9000122</div>
             <div className="border-t border-secondary my-4"></div>
-            <Nav></Nav>
+            <div>
+                <Nav></Nav>
+            </div>
         </header>
     );
 }
