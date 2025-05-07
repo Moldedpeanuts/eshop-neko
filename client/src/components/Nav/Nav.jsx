0f054528e893856d9b14b48ea4a74156a138cc74
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-05-07 21:08:05 +0300

    Make Nav Component mobile responsive

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -10,28 +10,31 @@ export default function Nav() {
     return(
         <>
              <div className="w-full">
-                <div className="flex justify-around px-4 py-4 gap-4">
+
+                 {/* --- Mobile View --- */}
+
+                <div className="flex flex-col md:flex-row sm:flex-row items-center justify-between px-4 py-4 gap-4">
                     {/* Logo Section */}
-                    <div className="flex justify-center gap-2">
+                    <div className="flex items-center gap-2 sm:w-3/4 md:w-2/3 lg:w-1/3">
                         <FaCat className="text-2xl cursor-pointer"></FaCat><h1 className="text-5xl font-bold cursor-pointer">NEKO</h1>
                     </div>
                     {/* Search Bar Section */}
-                    <div>
+                    <div className="sm:w-3/4 md:w-2/3 lg:w-1/3">
                         <input
                             type="text"
                             placeholder="Search..."
-                            className="w-full w-64 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-secondary border border-gray-300 text-black"
+                            className="w-full py-2 rounded focus:outline-none focus:ring-2 focus:ring-secondary border border-gray-300 text-black"
                         />
                     </div>
 
                     {/* Profile, Favorites and Cart section */}
-                    <div className="flex justify-center gap-4">
+                    <div className="sm:w-3/4 md:w-2/3 lg:w-1/3 flex justify-center gap-6">
                         <a href="#"><FaRegUser className="text-2xl"></FaRegUser></a>
                         <a href="#"><FaRegHeart className="text-2xl"></FaRegHeart></a>
                         <a href="#"><BsCart3 className="text-2xl"></BsCart3></a>
                     </div>
                 </div>
-                <div className="flex justify-start gap-2 px-4 bg-secondary py-4 font-bold">
+                <div className="flex items-center gap-2 px-4 bg-secondary py-4 font-bold">
                     <FaBars className="text-2xl cursor-pointer"></FaBars><h2 className="cursor-pointer">Categories</h2>
                 </div>
             </div>
