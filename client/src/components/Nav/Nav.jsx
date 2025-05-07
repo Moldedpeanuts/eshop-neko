4d5845f657c7cd2b521e3e33639fae0df3ad28e4
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-05-07 20:00:23 +0300

    Remove column layout for header element

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -9,32 +9,29 @@ import { FaBars } from 'react-icons/fa'; // menu icon
 export default function Nav() {
     return(
         <>
-             <div className="container mx-auto py-6">
-                <div className="grid grid-cols-3 gap-4 items-center">
+             <div className="w-full">
+                <div className="flex">
                     {/* Logo Section */}
-                    <div className="flex justify-start">
+                    <div>
                         <FaCat className="text-2xl cursor-pointer"></FaCat><h1 className="text-5xl font-bold cursor-pointer">NEKO</h1>
                     </div>
-
                     {/* Search Bar Section */}
-                    <div className="flex justify-center">
                         <input
                             type="text"
                             placeholder="Search..."
                             className="w-full w-64 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-secondary border border-gray-300 text-black"
                         />
-                    </div>
 
                     {/* Profile, Favorites and Cart section */}
-                    <div className="flex justify-end sm:gap-4 space-x-4">
+                    <div>
                         <a href="#"><FaRegUser className="text-2xl"></FaRegUser></a>
                         <a href="#"><FaRegHeart className="text-2xl"></FaRegHeart></a>
                         <a href="#"><BsCart3 className="text-2xl"></BsCart3></a>
                     </div>
                 </div>
-            </div>
-            <div className="flex justify-start gap-2 px-4 bg-secondary py-4 font-bold">
-               <FaBars className="text-2xl cursor-pointer"></FaBars><h2 className="cursor-pointer">Categories</h2>
+                <div className="flex justify-start gap-2 px-4 bg-secondary py-4 font-bold">
+                    <FaBars className="text-2xl cursor-pointer"></FaBars><h2 className="cursor-pointer">Categories</h2>
+                </div>
             </div>
         </>
     );
