e3f933d8041ebd9c1e2c9be86677ec46ea143fe1
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-05-11 11:38:38 +0300

    Add response status 409 if user is registered

--- server/myapp/routes/registration.js
+++ server/myapp/routes/registration.js
@@ -6,4 +6,8 @@ const { comparePassword } = require('../utils/password');
 router.post('/', async (req, res) => {
     const {firstName, lastName, email, password} = req.body;
     const userExists = await User.findOne({ email });
+
+    if(userExists) {
+        res.status(409).json({ error: 'Account already exists'});
+    }
 });
