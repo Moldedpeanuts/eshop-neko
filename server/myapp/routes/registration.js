a21b84be4ade81dfc6cfa3c9ecaac22b58ef717a
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-05-12 19:28:52 +0300

    Remove comparePassword dentrimental import

--- server/myapp/routes/registration.js
+++ server/myapp/routes/registration.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const { comparePassword } = require('../utils/password');
 
 router.post('/', async (req, res) => {
     const {firstName, lastName, email, password} = req.body;
