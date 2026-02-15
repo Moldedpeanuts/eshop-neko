1fda792401440d0fc79d8efab57312c3a00afb14
Sof <sofmakraki@gmail.com>
2026-02-15 17:53:39 +0200

    Hash password before register

--- server/myapp/models/User.js
+++ server/myapp/models/User.js
@@ -8,4 +8,14 @@ const userSchema = new Schema({
     password: { type: String, required: true },
 });
 
+userSchema.pre('save', async function(next){
+    let user = this;
+    if(!user.isModified('password')) {
+        return next();
+    }
+    const hashPassword = require('../utils/password').hashPassword;
+    this.password = await hashPassword(this.password);
+    next();
+});
+
 module.exports = mongoose.model('User', userSchema);
